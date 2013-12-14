c07b721fc8d1635d44a0fa1e94f456d4525943ac
ideksad <ideksad@hotmail.com>
2013-12-14 17:00:06 +0700

    JAVA RMI&testRMI
    นาย พัทธดนย์ จำเนียร
    544259128 54/94
    วิศวกรรมซอฟต์แวร์

--- rmi/src/RMIClient.java
+++ rmi/src/RMIClient.java
@@ -13,40 +13,40 @@ public class RMIClient {
 			int temp = obj.remoteMethod(Month);
 			switch(Month){
 			case 1:
-	            System.out.println("\nMonth : January");
+	            System.out.println("\nMonth : January = 31 Day");
 	            break;
 	        case 2:
-	            System.out.println("\nMonth : February");
+	            System.out.println("\nMonth : February = 28,29 Day");
 	            break;
 	        case 3:
-	            System.out.println("\nMonth : March");
+	            System.out.println("\nMonth : March = 31 Day");
 	            break;
 	        case 4:
-	            System.out.println("\nMonth :April");
+	            System.out.println("\nMonth :April = 30 Day");
 	            break;
 	        case 5:
-	            System.out.println("\nMonth :May");
+	            System.out.println("\nMonth :May = 31 Day");
 	            break;
 	        case 6:
-	            System.out.println("\nMonth :June");
+	            System.out.println("\nMonth :June = 30 Day");
 	            break;
 	        case 7:
-	            System.out.println("\nMonth :July");
+	            System.out.println("\nMonth :July = 31 Day");
 	            break;
 	        case 8:
-	            System.out.println("\nMonth :August");
+	            System.out.println("\nMonth :August = 31 Day");
 	            break;
 	        case 9:
-	            System.out.println("\nMonth :September");
+	            System.out.println("\nMonth :September = 30 Day");
 	            break;
 	        case 10:
-	            System.out.println("\nMonth :October");
+	            System.out.println("\nMonth :October = 31 Day");
 	            break;
 	        case 11:
-	            System.out.println("\nMonth :November");
+	            System.out.println("\nMonth :November = 30 Day");
 	            break;
 	        case 12:
-	            System.out.println("\nMonth :December");
+	            System.out.println("\nMonth :December = 31 Day");
 	            break;
 	        default:
 	        	System.out.println("\nError Enter " + Month + " isn't Mouth");
